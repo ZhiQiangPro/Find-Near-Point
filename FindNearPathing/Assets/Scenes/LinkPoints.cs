47c0bae9e6afecfb5d42e840e04966378e0fcb37
ZhiQiangPro <39686392+ZhiQiangPro@users.noreply.github.com>
2020-09-08 15:26:41 +0800

    Add files via upload

---
+++ FindNearPathing/Assets/Scenes/LinkPoints.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+public class LinkPoints : MonoBehaviour
+{
+    public List<Transform> points = new List<Transform>();
+    List<Transform> newPoints = new List<Transform>();
+
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            points.Add(transform.GetChild(i));
+        }
+        newPoints = PathFinding.GetInstance().GetNewList(points);
+        var newPointVec = new List<Vector3>();
+        for (int i = 0; i < newPoints.Count; i++)
+        {
+            newPoints[i].GetChild(0).GetComponent<TextMeshPro>().text = i.ToString();
+        }
+        foreach (var item in newPoints)
+        {
+            newPointVec.Add(item.position);
+        }
+        
+    }
+
+   
+}
---
+++ FindNearPathing/Assets/Scenes/PathFinding.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PathFinding : MonoBehaviour
+{
+    private static PathFinding instance;
+    public static PathFinding GetInstance()
+    {
+        return instance;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    /// <summary>
+    /// 获取新点,最后没有最近点之后 NextTransfrom添加了自身
+    /// </summary>
+    public List<Transform> GetNewList(List<Transform> pathList)
+    {
+        bool seeking = true;
+        List<Transform> newPath1List = new List<Transform>();
+        var nextNode = GetSmalldistanceVec(pathList[0], pathList);
+        nextNode.GetComponent<Node_zq>().Selected = true;
+        pathList[0].GetComponent<Node_zq>().NextTransfrom = nextNode;
+        pathList[0].GetComponent<Node_zq>().Selected = true;
+        newPath1List.Add(pathList[0]);
+        while (seeking)
+        {
+            Transform nextPoint = null;
+            foreach (var point in pathList)
+            {
+                if (point.GetComponent<Node_zq>().Selected == true && point.GetComponent<Node_zq>().NextTransfrom == null)
+                {
+                    nextPoint = point;
+                    break;
+                }
+                else
+                {
+                    nextPoint = null;
+                }
+            }
+            if (nextPoint != null)
+            {
+                nextPoint.GetComponent<Node_zq>().Selected = true;
+                var otherList = new List<Transform>();
+                foreach (var item in pathList)
+                {
+                    if (item.GetComponent<Node_zq>().Selected != true && item.GetComponent<Node_zq>().NextTransfrom == null)
+                    {
+                        otherList.Add(item);
+                    }
+                }
+                var zuiJinDian = GetSmalldistanceVec(nextPoint, otherList);
+                zuiJinDian.GetComponent<Node_zq>().Selected = true;
+                nextPoint.GetComponent<Node_zq>().NextTransfrom = zuiJinDian;
+                newPath1List.Add(nextPoint);
+            }
+
+            var panDuanList = new List<Transform>();
+            foreach (var item in pathList)
+            {
+                if (item.GetComponent<Node_zq>().Selected == true && item.GetComponent<Node_zq>().NextTransfrom != null)
+                {
+                    panDuanList.Add(item);
+                }
+            }
+            if (panDuanList.Count == pathList.Count)
+            {
+                seeking = false;
+            }
+
+        }
+        return newPath1List;
+
+    }
+
+    /// <summary>
+    /// 获取当前点的最近距离点,列表为空,返回自身
+    /// </summary>
+    /// <param name="thisObj"></param>
+    /// <param name="otherObjs"></param>
+    /// <returns></returns>
+    Transform GetSmalldistanceVec(Transform thisObj, List<Transform> otherObjs)
+    {
+        var distenceDc = new Dictionary<float, Transform>();
+        distenceDc.Clear();
+        foreach (var item in otherObjs)
+        {
+            var dis = Vector3.Distance(thisObj.position, item.position);
+            if (dis != 0)
+            {
+                distenceDc.Add(dis, item);
+            }
+        }
+        var shengYuSortDic = (from objDic in distenceDc orderby objDic.Key select objDic).ToDictionary(pair => pair.Key, Pair => Pair.Value);
+        if (shengYuSortDic.Count != 0)
+        {
+            return (shengYuSortDic.First().Value);
+
+        }
+        else
+        {
+            return (thisObj);
+
+        }
+
+    }
+
+}
